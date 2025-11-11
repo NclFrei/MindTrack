b80379b42b90f7a2a322cd20d1cc0f86fb503df0
NclFrei <nicollasfreieleoterio@gmail.com>
2025-11-11 03:34:32 -0300

    Adicionar arquivos de projeto.

---
+++ MindTrack/Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace MindTrack.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+    public void SetPassword(string password)
+    {
+        Password = BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool CheckPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, Password);
+    }
+}
---
+++ MindTrack/Infrastructure/Data/MindTrackContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MindTrack.Domain.Models;
+using System.IO;
+
+namespace MindTrack.Infrastructure.Data
+{
+    public class MindTrackContext : DbContext
+    {
+        public MindTrackContext(DbContextOptions<MindTrackContext> options) : base(options)
+        { }
+
+        public DbSet<User> User { get; set; }
+
+
+    }
+}
